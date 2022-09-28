892d11589485b27875f5c2f93bfa2c2cd1c6d7e4
Ryota Kota <ryota.kota@member.fsf.org>
2022-09-29 04:31:46 +0900

    setup extension from telescope.setup() only in ex_command_spec.lua

--- lua/tests/minimal_init.vim
+++ lua/tests/minimal_init.vim
@@ -1,18 +1,1 @@
 packloadall
-
-lua << EOF
-local telescope = require "telescope"
-telescope.setup({
-  extensions = {
-    menu = {
-      default = {
-        items = {
-          { display = "Check health", value="checkhealth"},
-          { action = "command", display = "Find files", value="Telescope find_files"},
-        }
-      }
-    }
-  }
-})
-telescope.load_extension("menu")
-EOF
--- lua/tests/telescope-menu/ex_command_spec.lua
+++ lua/tests/telescope-menu/ex_command_spec.lua
@@ -1,3 +1,22 @@
+-- setup
+(function()
+    local telescope = require "telescope"
+    telescope.setup({
+      extensions = {
+        menu = {
+          default = {
+            items = {
+              { display = "Check health", value="checkhealth"},
+              { action = "command", display = "Find files", value="Telescope find_files"},
+            }
+          },
+          menu2 = {}
+        }
+      }
+    })
+    telescope.load_extension("menu")
+end)()
+
 describe("integration tests:", function()
   it("Telescope menu", function()
     vim.cmd [[ Telescope menu ]]
